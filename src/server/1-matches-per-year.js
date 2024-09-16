2c90f8f960714785b93cb5a70eae3892811287e1
Jeevan Kumar <jeevankumar3229@gmail.com>
2024-09-16 11:54:14 +0530

    optimize code in 1-matches-per-year.js

--- src/server/1-matches-per-year.js
+++ src/server/1-matches-per-year.js
@@ -1,20 +1,12 @@
 import fs from "fs"
 export default function matchesPerYear(){
-    let object={};
+    let object={}
     let data=JSON.parse(fs.readFileSync('./src/data/matches.json','utf-8',(err,data)=>{if(err) console.log("Error")}));
-    for(let index=0;index<data.length;index++){
-        if(data[index].hasOwnProperty("season")){
-            if(object.hasOwnProperty([data[index]["season"]]))
-            {
-                object[data[index]["season"]]++;
-            }
-            else{
-                object[data[index]["season"]]=1;
-            }
+    data.forEach((item)=>{
+        if(item.hasOwnProperty("season")){
+            object[item["season"]]= object.hasOwnProperty(item["season"]) ? object[item["season"]]+1 : 1;
         }
-    }
+    })
     return object;
-    
-
 }
 
