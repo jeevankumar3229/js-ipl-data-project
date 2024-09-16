09e7d1f947736cc6745ae8db8b60cf6da00170a0
Jeevan Kumar <jeevankumar3229@gmail.com>
2024-09-16 14:24:46 +0530

    Implemented logic for 2-matches-per-year.js using the array built-in methods

--- src/server/2-matches-per-year-per-team.js
+++ src/server/2-matches-per-year-per-team.js
@@ -1,23 +1,20 @@
 import matchesPerYear from "./1-matches-per-year.js";
 import fs from 'fs';
 export default function matchesPerYearPerTeam(){
-    let output=matchesPerYear();  //This function returns the object with year as keys
     let object={};
+    let winner;
     let data=JSON.parse(fs.readFileSync('./src/data/matches.json','utf-8',(err)=>{if(err) console.log("Error")}));
-    for(let key in output){
-        object[key]={}
-        for(let index=0;index<data.length;index++){
-            if(data[index].hasOwnProperty("winner")){
-                if(data[index]["season"]===key){
-                    if(object[key].hasOwnProperty(data[index]["winner"])){
-                        object[key][data[index]["winner"]]++
-                    }
-                    else{
-                        object[key][data[index]["winner"]]=1
-                    }
+    data.forEach((item)=>{
+            if(item.hasOwnProperty('winner') && item.hasOwnProperty("season")){
+                winner= (item['winner']=== 'Rising Pune Supergiant' || item['winner']=== 'Rising Pune Supergiants') ? 'Rising Pune Supergiants' : item['winner']
+                if(object.hasOwnProperty(winner)){
+                    object[winner][item['season']]= object[winner].hasOwnProperty(item['season']) ? object[winner][item['season']] +1 : 1;
                 }
-            }
-        }
-    }
+                else{
+                    object[winner]={}
+                    object[winner][item['season']]=1;
+                }  
+            }    
+    });
     return object;
 }
