705f27d23c06f0338902399db0ed4f4065f19ce1
Jeevan Kumar <jeevankumar3229@gmail.com>
2024-09-16 14:40:03 +0530

    Modify 2-matches-per-year-per-team.js

--- src/server/2-matches-per-year-per-team.js
+++ src/server/2-matches-per-year-per-team.js
@@ -6,7 +6,7 @@ export default function matchesPerYearPerTeam(){
     let data=JSON.parse(fs.readFileSync('./src/data/matches.json','utf-8',(err)=>{if(err) console.log("Error")}));
     data.forEach((item)=>{
             if(item.hasOwnProperty('winner') && item.hasOwnProperty("season")){
-                winner= (item['winner']=== 'Rising Pune Supergiant' || item['winner']=== 'Rising Pune Supergiants') ? 'Rising Pune Supergiants' : item['winner']
+                winner= (item['winner']=== 'Rising Pune Supergiant' || item['winner']=== 'Rising Pune Supergiants') ? 'Rising Pune Supergiants' : (item['winner']==="" ? "Tie" : item['winner'])
                 if(object.hasOwnProperty(winner)){
                     object[winner][item['season']]= object[winner].hasOwnProperty(item['season']) ? object[winner][item['season']] +1 : 1;
                 }
