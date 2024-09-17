0231df28ceee3151d3e06b2f34897d97c7769482
Jeevan Kumar <jeevankumar3229@gmail.com>
2024-09-17 12:21:42 +0530

    Modify extraRunsPerTeam.js to pass the jsondata as argument to function and include try and catch block within it

--- src/public/testing/extraRunsPerTeam.js
+++ src/public/testing/extraRunsPerTeam.js
@@ -1,5 +1,12 @@
 import extraRunsPerTeam from "../../server/3-extra-runs-per-team.js";
 import fs from "fs";
-let output=extraRunsPerTeam()
+let matchesData=JSON.parse(fs.readFileSync('./src/data/matches.json','utf-8',(err)=>{if(err) console.log("Error")}));
+let deliveriesData=JSON.parse(fs.readFileSync('./src/data/deliveries.json','utf-8',(err)=>{if(err) console.log("Error")}));
+let output=extraRunsPerTeam(matchesData, deliveriesData)
 let jsondata=JSON.stringify(output,null,2)
-fs.writeFile('./src/public/output/extraRunsPerTeam.json',jsondata,(err)=>{if(err)console.log(err.message)});
+try{
+    fs.writeFile('./src/public/output/extraRunsPerTeam.json',jsondata,(err)=>{if(err)console.log(err.message)});
+}
+catch(Error){
+    console.log(Error)
+}
--- src/server/3-extra-runs-per-team.js
+++ src/server/3-extra-runs-per-team.js
@@ -1,12 +1,10 @@
 import fs from "fs"
-export default function extraRunsPerTeam(){
+export default function extraRunsPerTeam(matchesData, deliveriesData){
     let object={};
     let filteredArray=[]
     let matchID=[]
-    let data=JSON.parse(fs.readFileSync('./src/data/matches.json','utf-8',(err)=>{if(err) console.log("Error")}));
-    let data1=JSON.parse(fs.readFileSync('./src/data/deliveries.json','utf-8',(err)=>{if(err) console.log("Error")}));
-    matchID=data.filter((item)=> {if(item['season']==='2016') return item['id']}).map((item)=>item.id)
-    filteredArray = data1.filter((item)=> {if(matchID.includes(item['match_id'])) return item})
+    matchID=matchesData.filter((item)=> {if(item['season']==='2016') return item['id']}).map((item)=>item.id)
+    filteredArray = deliveriesData.filter((item)=> {if(matchID.includes(item['match_id'])) return item})
     filteredArray.forEach((item)=>{
         object[item['bowling_team']]= object.hasOwnProperty(item['bowling_team']) ? object[item['bowling_team']] + Number(item['extra_runs']) : Number(item['extra_runs']);
     })
