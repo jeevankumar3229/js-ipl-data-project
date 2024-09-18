2793df9df4226901bac3f2516e583796226bca89
Jeevan Kumar <jeevankumar3229@gmail.com>
2024-09-18 12:04:46 +0530

    Modify 6-highest-no-of-times-player-of-the match.js

--- src/public/testing/playerOfTheMatch.js
+++ src/public/testing/playerOfTheMatch.js
@@ -1,5 +1,11 @@
 import playerOfTheMatch from "../../server/6-highest-no-of-times-player-of-the-match.js";
 import fs from "fs";
-let output= playerOfTheMatch()
+let matchesData=JSON.parse(fs.readFileSync('./src/data/matches.json','utf-8',(err,data)=>{if(err) console.log("Error")}));
+let output= playerOfTheMatch(matchesData)
 let jsondata=JSON.stringify(output,null,2)
-fs.writeFile('./src/public/output/playerOfTheMatch.json',jsondata,(err)=>{if(err)console.log(err.message)});
+try{
+    fs.writeFile('./src/public/output/playerOfTheMatch.json',jsondata,(err)=>{if(err)console.log(err.message)});
+}
+catch(Error){
+    console.log(Error)
+}
--- src/server/6-highest-no-of-times-player-of-the-match.js
+++ src/server/6-highest-no-of-times-player-of-the-match.js
@@ -1,36 +1,27 @@
 import matchesPerYear from "./1-matches-per-year.js";
 import fs from 'fs';
-export default function playerOfTheMatch(){
-    let output=matchesPerYear();//This function returns the year as object
+export default function playerOfTheMatch(matchesData){
     let object={};
-    let data=JSON.parse(fs.readFileSync('./src/data/matches.json','utf-8',(err,data)=>{if(err) console.log("Error")}));
-    for(let key in output){
-        object[key]={}
-        for(let index=0;index<data.length;index++){
-            if(data[index].hasOwnProperty("player_of_match")){
-                if(data[index]["season"]===key){
-                    if(object[key].hasOwnProperty(data[index]["player_of_match"])){
-                        object[key][data[index]["player_of_match"]]++
-                    }
-                    else{
-                        object[key][data[index]["player_of_match"]]=1
-                    }
-                }
+    matchesData.forEach((item)=>{
+        if(item.hasOwnProperty('player_of_match') && item.hasOwnProperty("season")){
+            if(object.hasOwnProperty(item['season'])){
+                object[item['season']][item['player_of_match']]= object[item['season']].hasOwnProperty(item['player_of_match']) ? object[item['season']][item['player_of_match']] +1 : 1;
             }
-        }
-    }
-    let maxvalue=0;
-    let playerName="";
-    for(let key1 in object){
-        for(let key2 in object[key1]){
-            if(maxvalue<object[key1][key2]){
-                maxvalue=object[key1][key2]
-                playerName=key2
+            else{
+                object[item['season']]={}
+                object[item['season']][item['player_of_match']]=1;
+            }  
+        }    
+    });
+    let filteredObject=Object.entries(object).map(([season,players])=>{
+        let innerOutput=Object.entries(players).reduce((acc,[playername,count])=>{
+            if(acc["Count"] < count){
+                acc["Count"]=count
+                acc["playerName"]=playername
             }
-            
-        }
-        object[key1]={"name":playerName,"playerOfMatchCount": maxvalue}
-        maxvalue=0;
-    }
-    return object;
+            return acc;
+        },{"playerName":"","Count":0})
+        return [season,innerOutput]
+    })
+    return Object.fromEntries(filteredObject)
 }
