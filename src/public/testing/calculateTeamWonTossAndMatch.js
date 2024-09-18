a4fb14fba72be1d957d977f9684a655ee2c12008
Jeevan Kumar <jeevankumar3229@gmail.com>
2024-09-18 10:03:47 +0530

    Modify the 5-each-team-won-toss-and-game.js file using the array method foreach and Modify the calculateTeamWonTossAndMatch.js to include try and catch

--- src/public/testing/calculateTeamWonTossAndMatch.js
+++ src/public/testing/calculateTeamWonTossAndMatch.js
@@ -2,10 +2,10 @@ import calculateTeamWonTossAndMatch from "../../server/5-each-team-won-toss-and-
 import fs from "fs";
 let matchesData=JSON.parse(fs.readFileSync('./src/data/matches.json','utf-8',(err,data)=>{if(err) console.log("Error")}));
 let output= calculateTeamWonTossAndMatch(matchesData)
-/*let jsondata=JSON.stringify(output,null,2)
+let jsondata=JSON.stringify(output,null,2)
 try{
-    fs.writeFile('./src/public/output/teamWonTossAndMatch.json',jsondata,(err)=>{if(err)console.log(err.message)});
+    fs.writeFileSync('./src/public/output/teamWonTossAndMatch.json',jsondata);
 }
 catch(Error){
     console.log(Error)
-}*/
+}
--- src/server/5-each-team-won-toss-and-game.js
+++ src/server/5-each-team-won-toss-and-game.js
@@ -1,34 +1,15 @@
 import fs from "fs"
-export default function calculateTeamWonTossAndMatch(){
+export default function calculateTeamWonTossAndMatch(matchesData){
     let object={};
-    let data=JSON.parse(fs.readFileSync('./src/data/matches.json','utf-8',(err,data)=>{if(err) console.log("Error")}));
-    for(let index=0;index<data.length;index++){
-        if(data[index].hasOwnProperty("toss_winner") && data[index].hasOwnProperty("winner")){
-            if(data[index]['toss_winner']=== data[index]['winner']){
-                //since dataset contains team name as Rising Pune Supergiants and Rising Pune Supergiant, so merging both into one
-                if(data[index]['winner']=== 'Rising Pune Supergiant' || data[index]['winner']=== 'Rising Pune Supergiants'){
-                    let winner="Rising Pune Supergiants"
-                    if(object.hasOwnProperty(winner))
-                    {
-                        object[winner]++;
-                    }
-                    else{
-                        object[winner]=1;
-                    }
+    let winner;
+    matchesData.forEach((item)=>{
+            if(item.hasOwnProperty('winner') && item.hasOwnProperty("toss_winner")){
+                if(item['winner']===item['toss_winner']){
+                    winner= (item['winner']=== 'Rising Pune Supergiant' || item['winner']=== 'Rising Pune Supergiants') ? 'Rising Pune Supergiants' : (item['winner']==="" ? "Tie" : item['winner'])
+                    object[winner]= object.hasOwnProperty(winner) ? object[winner]+1 : 1;
                 }
-                else{
-                    if(object.hasOwnProperty([data[index]["winner"]]))
-                    {
-                        object[data[index]["winner"]]++;
-                    }
-                    else{
-                        object[data[index]["winner"]]=1;
-                    }
-                }
-            }
-        }
-    }
+            }    
+    });
     return object;
-    
 
 }
