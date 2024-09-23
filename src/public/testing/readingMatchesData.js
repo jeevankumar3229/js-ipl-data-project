264d69953a12937d4ca9a5109bbe8af75286c659
Jeevan Kumar <jeevankumar3229@gmail.com>
2024-09-23 12:28:32 +0530

    Create file readingMatchesData.js to add function to read matches data

---
+++ src/public/testing/readingMatchesData.js
@@ -0,0 +1,11 @@
+import fs from 'fs'
+export default function readingData(){
+    let matchesData
+    try{
+        matchesData=JSON.parse(fs.readFileSync('./src/data/matches.json','utf-8'));
+    }
+    catch(Error){
+        console.log(Error)
+    }
+    return matchesData
+}
