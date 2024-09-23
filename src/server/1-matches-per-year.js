eb7e00931e7e0c09c556bc875e6cd0d7df42e436
Jeevan Kumar <jeevankumar3229@gmail.com>
2024-09-23 14:50:18 +0530

    Add required comments to 1-matches-per-year.js

--- src/server/1-matches-per-year.js
+++ src/server/1-matches-per-year.js
@@ -1,7 +1,10 @@
+
+//This function will return count of no of matches played per year
 export default function matchesPerYear(matchesData) {
     try {
         let matchesPerYearData = matchesData.reduce((acc, item) => {
             if (item.hasOwnProperty("season")) {
+                //using ternery operator to assign value
                 acc[item["season"]] = acc.hasOwnProperty(item["season"]) ? acc[item["season"]] + 1 : 1;
             }
             return acc;
