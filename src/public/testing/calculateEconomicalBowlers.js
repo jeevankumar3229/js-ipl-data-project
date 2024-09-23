dfe36490fe0cc1d0cf835aaee380c3c2705c676b
Jeevan Kumar <jeevankumar3229@gmail.com>
2024-09-23 17:52:37 +0530

    Modify function using builtin array reduce method and modify variable name and add required comments

--- src/public/testing/calculateEconomicalBowlers.js
+++ src/public/testing/calculateEconomicalBowlers.js
@@ -1,11 +1,8 @@
 import calculateEconomicalBowlers from "../../server/4-top-ten-economical-bowlers.js";
-import fs from "fs";
-let matchesData=JSON.parse(fs.readFileSync('./src/data/matches.json','utf-8',(err)=>{if(err) console.log("Error")}));
-let deliveriesData=JSON.parse(fs.readFileSync('./src/data/deliveries.json','utf-8',(err)=>{if(err) console.log("Error")}));
-let output= calculateEconomicalBowlers(matchesData,deliveriesData)
-try{
-    fs.writeFileSync('./src/public/output/economicalBowlers.json',JSON.stringify(output,null,2))
-}
-catch(Error){
-    console.log(Error)
-}
+import readingData from "./readingMatchesData.js";
+import readingDeliveriesData from "./readingDeliveriesData.js";
+import writingData from "./writingData.js";
+let matchesData = readingData()//calling function to read matches data
+let deliveriesData = readingDeliveriesData()//calling function to read deliveries data
+let output = calculateEconomicalBowlers(matchesData, deliveriesData)//this function returns the top 10 economical bowlers in season 2015
+writingData('./src/public/output/economicalBowlers.json', JSON.stringify(output, null, 2))//calling function to write data in jsonfile
--- src/server/4-top-ten-economical-bowlers.js
+++ src/server/4-top-ten-economical-bowlers.js
@@ -1,42 +1,51 @@
-import fs from "fs"
-export default function calculateEconomicalBowlers(matchesData,deliveriesData,season='2015'){
+//This function returns the data of top 10 economical bowlers in season 2015
+export default function calculateEconomicalBowlers(matchesData, deliveriesData, season = '2015') {
+    try {
+        let bowlersData = {};
+        let economicalRateOfBowlers = []
+        // //storing the match_id of season 2015 in set
+        let matchIDArray = matchesData.reduce((acc, item) => {
+            if (item['season'] === season) {
+                acc.add(item["id"])
+            }
+            return acc;
+        }, new Set())
+        //filtering the data based on the match id present in set
+        let filteredArray = deliveriesData.filter(item => matchIDArray.has(item["match_id"]))
 
-    let object={};
-    let arrayOfObjects=[]
-    let idArray= matchesData.reduce((acc,item)=>{
-        if(item['season']===season){
-            acc.push(item["id"])
-        }
-    return acc;},[])
-    let filteredArray=deliveriesData.filter(item => idArray.includes(item["match_id"]))
-
-    filteredArray.forEach((item)=>{
-        if(item.hasOwnProperty("bowler") && item.hasOwnProperty('ball') && item.hasOwnProperty('total_runs') && item.hasOwnProperty('legbye_runs') && item.hasOwnProperty('bye_runs') && item.hasOwnProperty('wide_runs') && item.hasOwnProperty('noball_runs')){
-            if(object.hasOwnProperty(item['bowler'])){
-                    object[item['bowler']]["Balls"]=object[item['bowler']]["Balls"]+1
-                    object[item['bowler']]["Runs"]=object[item['bowler']]["Runs"]+Number(item['total_runs'])-Number(item['bye_runs'])-Number(item['legbye_runs'])
-                    if(Number(item['wide_runs'])>0 || Number(item['noball_runs'])>0){
-                        object[item['bowler']]["Balls"]= object[item['bowler']]["Balls"] - 1
+        //create the bowlers object with bowler name as key and value being nested object with properties as balls and runs
+        bowlersData = filteredArray.reduce((acc, item) => {
+            if (item.hasOwnProperty("bowler") && item.hasOwnProperty('ball') && item.hasOwnProperty('total_runs') && item.hasOwnProperty('legbye_runs') && item.hasOwnProperty('bye_runs') && item.hasOwnProperty('wide_runs') && item.hasOwnProperty('noball_runs')) {
+                if (acc.hasOwnProperty(item['bowler'])) {
+                    acc[item['bowler']]["Balls"] = acc[item['bowler']]["Balls"] + 1
+                    acc[item['bowler']]["Runs"] = acc[item['bowler']]["Runs"] + Number(item['total_runs']) - Number(item['bye_runs']) - Number(item['legbye_runs'])
+                    if (Number(item['wide_runs']) > 0 || Number(item['noball_runs']) > 0) {
+                        acc[item['bowler']]["Balls"] = acc[item['bowler']]["Balls"] - 1
                     }
-            }
-            else{
-                object[item['bowler']]={}
-                object[item['bowler']]["Balls"]=1
-                object[item['bowler']]["Runs"]=Number(item['total_runs'])-Number(item['bye_runs'])-Number(item['legbye_runs'])
-                if(Number(item['wide_runs'])>0 || Number(item['noball_runs'])>0){
-                    object[item['bowler']]["Balls"]= object[item['bowler']]["Balls"] - 1
                 }
+                else {
+                    acc[item['bowler']] = {}
+                    acc[item['bowler']]["Balls"] = 1
+                    acc[item['bowler']]["Runs"] = Number(item['total_runs']) - Number(item['bye_runs']) - Number(item['legbye_runs'])
+                    if (Number(item['wide_runs']) > 0 || Number(item['noball_runs']) > 0) {
+                        acc[item['bowler']]["Balls"] = acc[item['bowler']]["Balls"] - 1
+                    }
 
+                }
             }
-        }
-    })
-    for(let keys in object){
-        let over=((object[keys]['Balls'])/6)
-        let economic=((object[keys]['Runs'])/over)
-        arrayOfObjects.push({"name":keys,"rate":economic})
+            return acc;
+        }, {})
+        //iterating through bowlers data to get economical rate and push an object with properties as keys and rate to array
+        for (let keys in bowlersData) {
+            let over = ((bowlersData[keys]['Balls']) / 6)
+            let economic = ((bowlersData[keys]['Runs']) / over)
+            economicalRateOfBowlers.push({ "name": keys, "rate": economic })
 
+        }
+        return economicalRateOfBowlers.sort((a, b) => a.rate - b.rate).slice(0, 10);
     }
-    return arrayOfObjects.sort((a, b) => a.rate-b.rate).slice(0,10);
-    
+    catch (Error) {
+        console.log(Error)
+    }
 
 }
