45f7b69221d6a546825691e5c720d3b8fac8ebf2
Jeevan Kumar <jeevankumar3229@gmail.com>
2024-09-23 12:10:20 +0530

    create a function in readingDelieveriesData.js and readingMatchesData.js to read the matches and deliveries data

--- src/public/testing/matchesPerYear.js
+++ src/public/testing/matchesPerYear.js
@@ -1,11 +1,12 @@
 import matchesPerYear from "../../server/1-matches-per-year.js";
+import readingData from "./readingData.js";
 import fs from "fs";
-let matchesData=JSON.parse(fs.readFileSync('./src/data/matches.json','utf-8',(err,data)=>{if(err) console.log("Error")}));
-let output=matchesPerYear(matchesData)
-let jsondata=JSON.stringify(output,null,2)
-try{
-    fs.writeFileSync('./src/public/output/matchesPerYear.json',jsondata);
+let matchesData = readingData()
+let output = matchesPerYear(matchesData)
+let jsondata = JSON.stringify(output, null, 2)
+try {
+    fs.writeFileSync('./src/public/output/matchesPerYear.json', jsondata);
 }
-catch(Error){
+catch (Error) {
     console.log(Error)
 }
