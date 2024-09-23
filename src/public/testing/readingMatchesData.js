fafe8c33dc7158ab09e16f751598c669334a867b
Jeevan Kumar <jeevankumar3229@gmail.com>
2024-09-23 12:13:39 +0530

    Modify readingMatchesData.js

--- src/public/testing/readingMatchesData.js
+++ src/public/testing/readingMatchesData.js
@@ -1,14 +1,8 @@
 import fs from 'fs'
 export default function readingData(){
-    let matchesData,deliveriesData
+    let matchesData
     try{
         matchesData=JSON.parse(fs.readFileSync('./src/data/matches.json','utf-8'));
-    }
-    catch(Error){
-        console.log(Error)
-    }
-    try{
-        deliveriesData=JSON.parse(fs.readFileSync('./src/data/deliveries.json','utf-8'));
     }
     catch(Error){
         console.log(Error)
