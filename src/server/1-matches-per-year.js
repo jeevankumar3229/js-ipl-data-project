df72640c2c7ea9ad118347634769eeb8b5b24b0b
Jeevan Kumar <jeevankumar3229@gmail.com>
2024-09-23 12:40:52 +0530

    Modify 1-matches-per-year.js to include try catch

--- src/server/1-matches-per-year.js
+++ src/server/1-matches-per-year.js
@@ -1,10 +1,15 @@
 export default function matchesPerYear(matchesData) {
-    let matchesPerYearData=matchesData.reduce((acc,item) => {
-        if (item.hasOwnProperty("season")) {
-            acc[item["season"]] = acc.hasOwnProperty(item["season"]) ? acc[item["season"]] + 1 : 1;
-        }
-        return acc;
-    }, {})
-    return matchesPerYearData;
+    try {
+        let matchesPerYearData = matchesData.reduce((acc, item) => {
+            if (item.hasOwnProperty("season")) {
+                acc[item["season"]] = acc.hasOwnProperty(item["season"]) ? acc[item["season"]] + 1 : 1;
+            }
+            return acc;
+        }, {})
+        return matchesPerYearData;
+    }
+    catch (Error) {
+        console.log(Error)
+    }
 }
 
