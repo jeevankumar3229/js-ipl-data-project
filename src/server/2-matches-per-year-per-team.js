af2b675e23c1d08fdb67aa152265595d398d3d53
Jeevan Kumar <jeevankumar3229@gmail.com>
2024-09-23 15:25:34 +0530

    Add required comments to 2-matches-per-year-per-team.js

--- src/server/2-matches-per-year-per-team.js
+++ src/server/2-matches-per-year-per-team.js
@@ -1,14 +1,14 @@
 import matchesPerYear from "./1-matches-per-year.js";
 export default function matchesPerYearPerTeam(matchesData){
-    let matchesPerYearData=matchesPerYear(matchesData); 
+    let matchesPerYearData=matchesPerYear(matchesData); // calling function which returns the object in the form of no of matches played per year
     let matchesPerYearPerTeamData={};
     for(let key in matchesPerYearData){
         matchesPerYearPerTeamData[key]={}
         for(let index=0;index<matchesData.length;index++){
-            if(matchesData[index].hasOwnProperty("winner") && matchesData[index].hasOwnProperty("season")){
+            if(matchesData[index].hasOwnProperty("winner") && matchesData[index].hasOwnProperty("season")){//checking whether property season and winner exists
                 if(matchesData[index]["season"]===key){
                     if(matchesData[index]['winner']===""){
-                        let winner="Tie"
+                        let winner="Tie"//if winner propertie value is blank, initializing winner variable to value "Tie"
                         if(matchesPerYearPerTeamData[key].hasOwnProperty(winner)){
                             matchesPerYearPerTeamData[key][winner]++
                         }
