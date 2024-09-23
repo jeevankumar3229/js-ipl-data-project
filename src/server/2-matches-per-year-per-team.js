aa24938b2a46fc07d5702730319f3df0324115f4
Jeevan Kumar <jeevankumar3229@gmail.com>
2024-09-23 16:39:07 +0530

    Add try catch code

--- src/server/2-matches-per-year-per-team.js
+++ src/server/2-matches-per-year-per-team.js
@@ -1,19 +1,24 @@
-export default function matchesPerYearPerTeam(matchesData){
-    let matchesPerYearPerTeamData={};
-    let winner;
-    matchesPerYearPerTeamData=matchesData.reduce((acc,item)=>{
-            if(item.hasOwnProperty('winner') && item.hasOwnProperty("season")){
+export default function matchesPerYearPerTeam(matchesData) {
+    try {
+        let matchesPerYearPerTeamData = {};
+        let winner;
+        matchesPerYearPerTeamData = matchesData.reduce((acc, item) => {
+            if (item.hasOwnProperty('winner') && item.hasOwnProperty("season")) {
                 //if the value of item['winner'] is empty, initialize the variable winner to tie
-                winner= (item['winner']=== 'Rising Pune Supergiant' || item['winner']=== 'Rising Pune Supergiants') ? 'Rising Pune Supergiants' : (item['winner']==="" ? "Tie" : item['winner'])
-                if(acc.hasOwnProperty(winner)){
-                    acc[winner][item['season']]= acc[winner].hasOwnProperty(item['season']) ? acc[winner][item['season']] +1 : 1;
+                winner = (item['winner'] === 'Rising Pune Supergiant' || item['winner'] === 'Rising Pune Supergiants') ? 'Rising Pune Supergiants' : (item['winner'] === "" ? "Tie" : item['winner'])
+                if (acc.hasOwnProperty(winner)) {
+                    acc[winner][item['season']] = acc[winner].hasOwnProperty(item['season']) ? acc[winner][item['season']] + 1 : 1;
                 }
-                else{
-                    acc[winner]={}//if the acc object doesn't contain the property with value of winner variable ,initialize to new empty object
-                    acc[winner][item['season']]=1;
-                }  
-            } 
-            return acc;   
-    },{});
-    return matchesPerYearPerTeamData;
+                else {
+                    acc[winner] = {}//if the acc object doesn't contain the property with value of winner variable ,initialize to new empty object
+                    acc[winner][item['season']] = 1;
+                }
+            }
+            return acc;
+        }, {});
+        return matchesPerYearPerTeamData;
+    }
+    catch (Error) {
+        console.log(Error)
+    }
 }
--- src/server/3-extra-runs-per-team.js
+++ src/server/3-extra-runs-per-team.js
@@ -1,23 +1,28 @@
 //This function will return the data of no of extra runs conceded by each team in 2016
-export default function extraRunsPerTeam(matchesData, deliveriesData, season='2016'){
-    let extraRunsPerTeamData;
-    let filteredArray=[]
-    let matchID=new Set()
-    //storing the match_id of season 2016 in set
-    matchID=matchesData.reduce((acc,item)=>{
-        if(item.hasOwnProperty('season') && item['season']===season){
-            acc.add(item["id"])
-        }
-        return acc;
-    },new Set());
-    //filtering the data based on the match id present in set
-    filteredArray = deliveriesData.filter((item)=> {if(matchID.has(item['match_id'])) return item})
-    extraRunsPerTeamData=filteredArray.reduce((acc,item)=>{
-        if(item.hasOwnProperty('bowling_team')){
-        acc[item['bowling_team']]= acc.hasOwnProperty(item['bowling_team']) ? acc[item['bowling_team']] + Number(item['extra_runs']) : Number(item['extra_runs']);
-        }
-        return acc;
-    },{})
-    return extraRunsPerTeamData;
-    
+export default function extraRunsPerTeam(matchesData, deliveriesData, season = '2016') {
+    try {
+        let extraRunsPerTeamData;
+        let filteredArray = []
+        let matchID = new Set()
+        //storing the match_id of season 2016 in set
+        matchID = matchesData.reduce((acc, item) => {
+            if (item.hasOwnProperty('season') && item['season'] === season) {
+                acc.add(item["id"])
+            }
+            return acc;
+        }, new Set());
+        //filtering the data based on the match id present in set
+        filteredArray = deliveriesData.filter((item) => { if (matchID.has(item['match_id'])) return item })
+        extraRunsPerTeamData = filteredArray.reduce((acc, item) => {
+            if (item.hasOwnProperty('bowling_team')) {
+                acc[item['bowling_team']] = acc.hasOwnProperty(item['bowling_team']) ? acc[item['bowling_team']] + Number(item['extra_runs']) : Number(item['extra_runs']);
+            }
+            return acc;
+        }, {})
+        return extraRunsPerTeamData;
+    }
+    catch (Error) {
+        console.log(Error)
+    }
+
 }
