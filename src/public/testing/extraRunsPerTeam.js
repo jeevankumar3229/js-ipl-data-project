46d5c13aa3e6a695d065239ff905840ac25d41f5
Jeevan Kumar <jeevankumar3229@gmail.com>
2024-09-23 16:17:13 +0530

    Modify the function defined in 3-extra-runs-per-team.js using array reduce methods and modifying the variable names

--- src/public/testing/extraRunsPerTeam.js
+++ src/public/testing/extraRunsPerTeam.js
@@ -1,12 +1,9 @@
 import extraRunsPerTeam from "../../server/3-extra-runs-per-team.js";
-import fs from "fs";
-let matchesData=JSON.parse(fs.readFileSync('./src/data/matches.json','utf-8',(err)=>{if(err) console.log("Error")}));
-let deliveriesData=JSON.parse(fs.readFileSync('./src/data/deliveries.json','utf-8',(err)=>{if(err) console.log("Error")}));
-let output=extraRunsPerTeam(matchesData, deliveriesData)
+import readingDeliveriesData from "./readingDeliveriesData.js";
+import readingData from "./readingMatchesData.js";
+import writingData from "./writingData.js";
+let matchesData= readingData()//calling function which reads and returns matches data
+let deliveriesData= readingDeliveriesData()//calling function which reads and returns the deliveries data
+let output=extraRunsPerTeam(matchesData, deliveriesData)//calling function which returns the no of extra runs conceded by each team in 2016
 let jsondata=JSON.stringify(output,null,2)
-try{
-    fs.writeFileSync('./src/public/output/extraRunsPerTeam.json',jsondata);
-}
-catch(Error){
-    console.log(Error)
-}
+writingData('./src/public/output/extraRunsPerTeam.json',jsondata)//calling function to write data to the json file
--- src/server/3-extra-runs-per-team.js
+++ src/server/3-extra-runs-per-team.js
@@ -1,18 +1,23 @@
-export default function extraRunsPerTeam(matchesData, deliveriesData,season='2016'){
-    let object={};
+//This function will return the data of no of extra runs conceded by each team in 2016
+export default function extraRunsPerTeam(matchesData, deliveriesData, season='2016'){
+    let extraRunsPerTeamData;
     let filteredArray=[]
     let matchID=new Set()
-    matchesData.forEach((item)=>{
+    //storing the match_id of season 2016 in set
+    matchID=matchesData.reduce((acc,item)=>{
         if(item.hasOwnProperty('season') && item['season']===season){
-            matchID.add(item["id"])
+            acc.add(item["id"])
         }
-    });
+        return acc;
+    },new Set());
+    //filtering the data based on the match id present in set
     filteredArray = deliveriesData.filter((item)=> {if(matchID.has(item['match_id'])) return item})
-    filteredArray.forEach((item)=>{
+    extraRunsPerTeamData=filteredArray.reduce((acc,item)=>{
         if(item.hasOwnProperty('bowling_team')){
-        object[item['bowling_team']]= object.hasOwnProperty(item['bowling_team']) ? object[item['bowling_team']] + Number(item['extra_runs']) : Number(item['extra_runs']);
+        acc[item['bowling_team']]= acc.hasOwnProperty(item['bowling_team']) ? acc[item['bowling_team']] + Number(item['extra_runs']) : Number(item['extra_runs']);
         }
-    })
-    return object;
+        return acc;
+    },{})
+    return extraRunsPerTeamData;
     
 }
