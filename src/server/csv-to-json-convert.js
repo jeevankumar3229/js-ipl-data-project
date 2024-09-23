c6bc5e33c50ffa251d8c79c769388c8746e958eb
Jeevan Kumar <jeevankumar3229@gmail.com>
2024-09-23 14:47:21 +0530

    Modify variable names in csv-to-json-convert.js

--- src/server/csv-to-json-convert.js
+++ src/server/csv-to-json-convert.js
@@ -18,12 +18,12 @@ function convertingCsvToJson(){
     });
 
 
-    csvFilePath='./src/data/deliveries.csv'
-    jsonFilePath='./src/data/deliveries.json'
+    let csvFilePath1='./src/data/deliveries.csv'
+    let jsonFilePath1='./src/data/deliveries.json'
     //reading csv file and writing data in json format to jsonfile
-    csv().fromFile(csvFilePath).then((jsonObj) => {
+    csv().fromFile(csvFilePath1).then((jsonObj) => {
         try{
-            fs.writeFileSync(jsonFilePath, JSON.stringify(jsonObj,null,2));
+            fs.writeFileSync(jsonFilePath1, JSON.stringify(jsonObj,null,2));
         }
         catch(Error){
             console.log(Error)
