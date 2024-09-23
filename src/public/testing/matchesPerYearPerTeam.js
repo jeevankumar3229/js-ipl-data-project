bc1654a31db699363c6ec404d004bcc86aed9b57
Jeevan Kumar <jeevankumar3229@gmail.com>
2024-09-23 14:24:38 +0530

    Add required comments

--- src/public/testing/matchesPerYearPerTeam.js
+++ src/public/testing/matchesPerYearPerTeam.js
@@ -1,5 +1,8 @@
 import matchesPerYearPerTeam from "../../server/2-matches-per-year-per-team.js";
-import fs from "fs";
-let output=matchesPerYearPerTeam()
-let jsondata=JSON.stringify(output,null,2)
-fs.writeFile('./src/public/output/matchesPerYearPerTeam.json',jsondata,(err)=>{if(err)console.log(err.message)});
+import readingData from "./readingMatchesData.js";
+import writingData from "./writingData.js";
+let matchesData = readingData()//calling readingData function to read matches data
+let output = matchesPerYearPerTeam(matchesData)//calling matchesPerYearPerTeam function and this return the count of the matched played by teams per year
+let jsonData = JSON.stringify(output, null, 2)
+writingData('./src/public/output/matchesPerYearPerTeam.json', jsonData);//calling writingData function to write data to json file
+
