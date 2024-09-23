d8b29ae41dc85f64578bdb3c1fabe67799ef21ef
Jeevan Kumar <jeevankumar3229@gmail.com>
2024-09-23 14:54:39 +0530

    Add required comments to 1-matches-per-year.js file

--- src/server/1-matches-per-year.js
+++ src/server/1-matches-per-year.js
@@ -1,7 +1,8 @@
+//This function will return count of no of matches played per year
 export default function matchesPerYear(matchesData){
     let matchesPerYearData={};
     for(let index=0;index<matchesData.length;index++){
-        if(matchesData[index].hasOwnProperty("season")){
+        if(matchesData[index].hasOwnProperty("season")){//checking whether season property exists
             if(matchesPerYearData.hasOwnProperty([matchesData[index]["season"]]))
             {
                 matchesPerYearData[matchesData[index]["season"]]++;
