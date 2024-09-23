1634ae41e4ed231376bfdc98552964d0b194f9e1
Jeevan Kumar <jeevankumar3229@gmail.com>
2024-09-23 14:14:15 +0530

    Modify 2-matches-per-year-per-team.js file function variables

--- src/server/2-matches-per-year-per-team.js
+++ src/server/2-matches-per-year-per-team.js
@@ -1,34 +1,32 @@
 import matchesPerYear from "./1-matches-per-year.js";
-import fs from 'fs';
-export default function matchesPerYearPerTeam(){
-    let output=matchesPerYear();  //This function returns the object with year as keys
-    let object={};
-    let data=JSON.parse(fs.readFileSync('./src/data/matches.json','utf-8',(err)=>{if(err) console.log("Error")}));
-    for(let key in output){
-        object[key]={}
-        for(let index=0;index<data.length;index++){
-            if(data[index].hasOwnProperty("winner")){
-                if(data[index]["season"]===key){
-                    if(data[index]['winner']===""){
+export default function matchesPerYearPerTeam(matchesData){
+    let matchesPerYearData=matchesPerYear(matchesData); 
+    let matchesPerYearPerTeamData={};
+    for(let key in matchesPerYearData){
+        matchesPerYearPerTeamData[key]={}
+        for(let index=0;index<matchesData.length;index++){
+            if(matchesData[index].hasOwnProperty("winner") && matchesData[index].hasOwnProperty("season")){
+                if(matchesData[index]["season"]===key){
+                    if(matchesData[index]['winner']===""){
                         let winner="Tie"
-                        if(object[key].hasOwnProperty(winner)){
-                            object[key][winner]++
+                        if(matchesPerYearPerTeamData[key].hasOwnProperty(winner)){
+                            matchesPerYearPerTeamData[key][winner]++
                         }
                         else{
-                            object[key][winner]=1
+                            matchesPerYearPerTeamData[key][winner]=1
                         }
                     }
                     else{
-                        if(object[key].hasOwnProperty(data[index]["winner"])){
-                            object[key][data[index]["winner"]]++
+                        if(matchesPerYearPerTeamData[key].hasOwnProperty(data[index]["winner"])){
+                            matchesPerYearPerTeamData[key][data[index]["winner"]]++
                         }
                         else{
-                            object[key][data[index]["winner"]]=1
+                            matchesPerYearPerTeamData[key][data[index]["winner"]]=1
                         }
                     }
                 }
             }
         }
     }
-    return object;
+    return matchesPerYearPerTeamData;
 }
