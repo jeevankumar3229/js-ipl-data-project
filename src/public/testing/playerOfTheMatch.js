4fc06757948a50e767b98395c84bf9cb00dd4864
Jeevan Kumar <jeevankumar3229@gmail.com>
2024-09-24 11:47:30 +0530

    Modify function using array reduce methods, modify variable names, add required comments and add try catch

--- src/public/testing/playerOfTheMatch.js
+++ src/public/testing/playerOfTheMatch.js
@@ -1,5 +1,7 @@
 import playerOfTheMatch from "../../server/6-highest-no-of-times-player-of-the-match.js";
-import fs from "fs";
-let output= playerOfTheMatch()
-let jsondata=JSON.stringify(output,null,2)
-fs.writeFile('./src/public/output/playerOfTheMatch.json',jsondata,(err)=>{if(err)console.log(err.message)});
+import readingData from "./readingMatchesData.js";
+import writingData from "./writingData.js";
+let matchesData = readingData()//calling the function to read matches data
+let output = playerOfTheMatch(matchesData)//this function returns the highest no of times single player won man of the match
+let jsondata = JSON.stringify(output, null, 2)
+writingData('./src/public/output/playerOfTheMatch.json', jsondata)//calling function to write data to json file
--- src/server/6-highest-no-of-times-player-of-the-match.js
+++ src/server/6-highest-no-of-times-player-of-the-match.js
@@ -1,36 +1,42 @@
-import matchesPerYear from "./1-matches-per-year.js";
-import fs from 'fs';
-export default function playerOfTheMatch(){
-    let output=matchesPerYear();//This function returns the year as object
-    let object={};
-    let data=JSON.parse(fs.readFileSync('./src/data/matches.json','utf-8',(err,data)=>{if(err) console.log("Error")}));
-    for(let key in output){
-        object[key]={}
-        for(let index=0;index<data.length;index++){
-            if(data[index].hasOwnProperty("player_of_match")){
-                if(data[index]["season"]===key){
-                    if(object[key].hasOwnProperty(data[index]["player_of_match"])){
-                        object[key][data[index]["player_of_match"]]++
-                    }
-                    else{
-                        object[key][data[index]["player_of_match"]]=1
+//this function returns the highest no of times single player won man of the match
+import matchesPerYear from './1-matches-per-year.js'
+export default function playerOfTheMatch(matchesData) {
+    try {
+        let seasonListData = matchesPerYear(matchesData);//This function returns the object with key being the year
+        let noOfTimesPlayerOfMatchData = {};
+        //noOfTimesPlayerOfTheMatch contains season as key and value being the nested object where the nested object contains the batsman name as key and value being no of times player won man of the match in that season
+        for (let key in seasonListData) {
+            noOfTimesPlayerOfMatchData[key] = {}
+            for (let index = 0; index < matchesData.length; index++) {
+                if (matchesData[index].hasOwnProperty("player_of_match") && matchesData[index].hasOwnProperty('season')) {
+                    if (matchesData[index]["season"] === key) {
+                        if (noOfTimesPlayerOfMatchData[key].hasOwnProperty(matchesData[index]["player_of_match"])) {
+                            noOfTimesPlayerOfMatchData[key][matchesData[index]["player_of_match"]]++
+                        }
+                        else {
+                            noOfTimesPlayerOfMatchData[key][matchesData[index]["player_of_match"]] = 1
+                        }
                     }
                 }
             }
         }
-    }
-    let maxvalue=0;
-    let playerName="";
-    for(let key1 in object){
-        for(let key2 in object[key1]){
-            if(maxvalue<object[key1][key2]){
-                maxvalue=object[key1][key2]
-                playerName=key2
+        //converting previously created object with value being nested object with mutliple key value pairs to single key value pairs
+        let highestPlayerOfMatchCount = 0;
+        let playerName = "";
+        for (let key1 in noOfTimesPlayerOfMatchData) {
+            for (let key2 in noOfTimesPlayerOfMatchData[key1]) {
+                if (highestPlayerOfMatchCount < noOfTimesPlayerOfMatchData[key1][key2]) {
+                    highestPlayerOfMatchCount = noOfTimesPlayerOfMatchData[key1][key2]
+                    playerName = key2
+                }
+
             }
-            
+            noOfTimesPlayerOfMatchData[key1] = { "name": playerName, "playerOfMatchCount": highestPlayerOfMatchCount }
+            highestPlayerOfMatchCount = 0;
         }
-        object[key1]={"name":playerName,"playerOfMatchCount": maxvalue}
-        maxvalue=0;
+        return noOfTimesPlayerOfMatchData;
     }
-    return object;
+    catch (Error) {
+        console.log(Error)
+    }
 }
