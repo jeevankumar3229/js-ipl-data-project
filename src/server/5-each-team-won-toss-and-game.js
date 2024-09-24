c6fb251fdb8bd9b950344286b539315940e5b66a
Jeevan Kumar <jeevankumar3229@gmail.com>
2024-09-24 10:37:43 +0530

    Add required comments

--- src/server/5-each-team-won-toss-and-game.js
+++ src/server/5-each-team-won-toss-and-game.js
@@ -1,10 +1,11 @@
+//This function will return the no of times each team won the toss as well as match
 export default function calculateTeamWonTossAndMatch(matchesData) {
     try {
         let TeamWonTossAndMatchData;
         let winner;
         TeamWonTossAndMatchData = matchesData.reduce((acc, item) => {
             if (item.hasOwnProperty('winner') && item.hasOwnProperty("toss_winner")) {
-                if (item['winner'] === item['toss_winner']) {
+                if (item['winner'] === item['toss_winner']) {//if value of winner column is empty, assign the variable winner with value "Tie"
                     winner = (item['winner'] === 'Rising Pune Supergiant' || item['winner'] === 'Rising Pune Supergiants') ? 'Rising Pune Supergiants' : (item['winner'] === "" ? "Tie" : item['winner'])
                     acc[winner] = acc.hasOwnProperty(winner) ? acc[winner] + 1 : 1;
                 }
