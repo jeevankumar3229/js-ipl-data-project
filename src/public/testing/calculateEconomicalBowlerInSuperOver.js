b008dbe1746cfff4c52154464835a06290fc35df
Jeevan Kumar <jeevankumar3229@gmail.com>
2024-09-24 23:11:19 +0530

    Remove unnecessary code

--- src/public/testing/calculateEconomicalBowlerInSuperOver.js
+++ src/public/testing/calculateEconomicalBowlerInSuperOver.js
@@ -1,8 +1,8 @@
-import calculateEconomicalBowlers from "../../server/4-top-ten-economical-bowlers.js";
+import calculateEconomicalBowlerInSuperOver from "../../server/9-bowler-with-best-economy-in-superover.js";
 import readingData from "./readingMatchesData.js";
 import readingDeliveriesData from "./readingDeliveriesData.js";
 import writingData from "./writingData.js";
-let matchesData = readingData()//calling function to read matches data
-let deliveriesData = readingDeliveriesData()//calling function to read deliveries data
-let output = calculateEconomicalBowlers(matchesData, deliveriesData)//this function returns the top 10 economical bowlers in season 2015
-writingData('./src/public/output/economicalBowlers.json', JSON.stringify(output, null, 2))//calling function to write data in jsonfile
+let deliveriesData= readingDeliveriesData()//calling function to read deliveries data
+let output= calculateEconomicalBowlerInSuperOver(deliveriesData)//this function returns the data of economical rate of bowlers in super over
+let jsondata=JSON.stringify(output,null,2)
+writingData('./src/public/output/economicalBowlersInSuperOver.json',jsondata)//calling function to write data to json file
