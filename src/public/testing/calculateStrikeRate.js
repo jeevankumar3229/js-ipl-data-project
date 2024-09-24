5b776daa8f95d6ecd0eade1858a59d087fcdbb03
Jeevan Kumar <jeevankumar3229@gmail.com>
2024-09-24 21:20:31 +0530

    Modify function using array reduce method, modify variable names, add required comments and add try catch code

--- src/public/testing/calculateStrikeRate.js
+++ src/public/testing/calculateStrikeRate.js
@@ -1,5 +1,9 @@
 import calculateStrikeRate from "../../server/7-strike-rate-of-batsman.js";
-import fs from "fs";
-let output= calculateStrikeRate()
+import readingDeliveriesData from "./readingDeliveriesData.js";
+import readingData from "./readingMatchesData.js";
+import writingData from "./writingData.js";
+let matchesData= readingData()//calling function to read matches data
+let deliveriesData= readingDeliveriesData()//calling function to read deliveries data
+let output= calculateStrikeRate(matchesData,deliveriesData)//this function returns the strike rate of batsman in each season
 let jsondata=JSON.stringify(output,null,2)
-fs.writeFile('./src/public/output/batsmanStrikeRate.json',jsondata,(err)=>{if(err)console.log(err.message)});
+writingData('./src/public/output/batsmanStrikeRate.json',jsondata)//calking function to write data to json file
--- src/server/7-strike-rate-of-batsman.js
+++ src/server/7-strike-rate-of-batsman.js
@@ -1,91 +1,74 @@
+//This function calculates the strike rate of batsman in each season
 import matchesPerYear from "./1-matches-per-year.js";
-import fs from 'fs';
-export default function calculateStrikeRate(){
-    let output=matchesPerYear();//This functions returns the object with year as key
-    let object={};
-    let object1={};
-    let data=JSON.parse(fs.readFileSync('./src/data/matches.json','utf-8',(err)=>{if(err) console.log("Error")}));
-    let data1=JSON.parse(fs.readFileSync('./src/data/deliveries.json','utf-8',(err)=>{if(err) console.log("Error")}));
-    for(let key in output){
-        object[key]={}
-        object1[key]={}
-        let idArray=[]
-        let batsman=[]
-        //pushing the matchid of each season to array
-        for(let index=0;index<data.length;index++){
-            if(data[index].hasOwnProperty("id") && data[index].hasOwnProperty("season")){
-                if(data[index]['season']=== key)
-                {
-                    idArray.push(data[index]["id"])
-                }
-            }
-        }
-        //pushing the name of the batsman playing in each season
-        for(let index1=0;index1<data1.length;index1++){
-            if(idArray.includes(data1[index1]["match_id"])){
-                if(data1[index1].hasOwnProperty("batsman")){
-                    if(batsman.includes(data1[index1]['batsman'])){
-                        continue
-                    }
-                    else{
-                        batsman.push(data1[index1]['batsman'])
+export default function calculateStrikeRate(matchesData, deliveriesData) {
+    try {
+        let seasonListData = matchesPerYear(matchesData);//This functions returns the object with year as key
+        let batsmanStrikeRateData = {};
+        for (let key in seasonListData) {
+            batsmanStrikeRateData[key] = {}
+            let matchIDArray = []
+            let batsmanData = []
+            //pushing the matchid of each season to array
+            for (let index = 0; index < matchesData.length; index++) {
+                if (matchesData[index].hasOwnProperty("id") && matchesData[index].hasOwnProperty("season")) {
+                    if (matchesData[index]['season'] === key) {
+                        matchIDArray.push(matchesData[index]["id"])
                     }
                 }
             }
-        }
-        for(let index2=0;index2<batsman.length;index2++){
-            object[key][batsman[index2]]={}
-            for(let index3=0;index3<data1.length;index3++){
-                if(idArray.includes(data1[index3]["match_id"])){
-                    if(data1[index3].hasOwnProperty("batsman")){
-                        if(data1[index3]["batsman"]=== batsman[index2]){
-                            if(object[key][batsman[index2]].hasOwnProperty("Balls") && object[key][batsman[index2]].hasOwnProperty("Runs")){
-                                object[key][batsman[index2]]["Balls"]=object[key][batsman[index2]]["Balls"]+1
-                                object[key][batsman[index2]]["Runs"]=object[key][batsman[index2]]["Runs"]+Number(data1[index3]['batsman_runs'])
-                                if(Number(data1[index3]['wide_runs'])>0){
-                                    object[key][batsman[index2]]["Wide"]=object[key][batsman[index2]]["Wide"]+1
+            //pushing the name of the batsman playing in each season to array
+            for (let index = 0; index < deliveriesData.length; index++) {
+                if (matchIDArray.includes(deliveriesData[index]["match_id"])) {
+                    if (deliveriesData[index].hasOwnProperty("batsman")) {
+                        if (batsmanData.includes(deliveriesData[index]['batsman'])) {
+                            continue
+                        }
+                        else {
+                            batsmanData.push(deliveriesData[index]['batsman'])
+                        }
+                    }
+                }
+            }
+            //creating a new object for each batsman with keys being balls and runs
+            for (let index = 0; index < batsmanData.length; index++) {
+                batsmanStrikeRateData[key][batsmanData[index]] = {}
+                for (let index1 = 0; index1 < deliveriesData.length; index1++) {
+                    if (matchIDArray.includes(deliveriesData[index1]["match_id"])) {
+                        if (deliveriesData[index1].hasOwnProperty("batsman") && deliveriesData[index1].hasOwnProperty('batsman_runs') && deliveriesData[index1].hasOwnProperty("wide_runs") && deliveriesData[index1].hasOwnProperty("noball_runs") && deliveriesData[index1].hasOwnProperty('ball')) {
+                            if (deliveriesData[index1]['batsman'] === batsmanData[index]) {
+                                if (batsmanStrikeRateData[key][deliveriesData[index1]['batsman']].hasOwnProperty("Balls")) {
+                                    batsmanStrikeRateData[key][deliveriesData[index1]['batsman']]["Balls"] = batsmanStrikeRateData[key][deliveriesData[index1]['batsman']]["Balls"] + 1
+                                    batsmanStrikeRateData[key][deliveriesData[index1]['batsman']]["Runs"] = batsmanStrikeRateData[key][deliveriesData[index1]['batsman']]["Runs"] + Number(deliveriesData[index1]['batsman_runs'])
+                                    if (Number(deliveriesData[index1]['wide_runs']) > 0) {
+                                        batsmanStrikeRateData[key][deliveriesData[index1]['batsman']]["Balls"] = batsmanStrikeRateData[key][deliveriesData[index1]['batsman']]["Balls"] - 1;
+                                    }
                                 }
-                                if(Number(data1[index3]['noball_runs'])>0){
-                                    object[key][batsman[index2]]['Noball']=object[key][batsman[index2]]['Noball']+1
+                                else {
+                                    batsmanStrikeRateData[key][deliveriesData[index1]['batsman']]["Balls"] = 1
+                                    batsmanStrikeRateData[key][deliveriesData[index1]['batsman']]["Runs"] = Number(deliveriesData[index1]['batsman_runs'])
+                                    if (Number(deliveriesData[index1]['wide_runs']) > 0) {
+                                        batsmanStrikeRateData[key][deliveriesData[index1]['batsman']]["Balls"] = batsmanStrikeRateData[key][deliveriesData[index1]['batsman']]["Balls"] - 1
+                                    }
+
                                 }
-                            }
-                            else{
-                                object[key][batsman[index2]]["Balls"]=1
-                                object[key][batsman[index2]]["Runs"]= Number(data1[index3]['batsman_runs'])
-                                if(Number(data1[index3]['wide_runs'])>0){
-                                    object[key][batsman[index2]]["Wide"]=1
-                                }
-                                else{
-                                    object[key][batsman[index2]]["Wide"]=Number(data1[index3]['wide_runs'])
-                                }
-                                if(Number(data1[index3]['noball_runs'])>0){
-                                    object[key][batsman[index2]]["Noball"]=1
-                                }
-                                else{
-                                    object[key][batsman[index2]]['Noball']=Number(data1[index3]['noball_runs'])
-    
-                                }
-    
                             }
                         }
-                        
-                        
                     }
                 }
+
             }
-                    
-                    
-                
-            
+            //modifying the object created for each batsman by replacing the object with strikerate value
+            for (let keys in batsmanStrikeRateData[key]) {
+                let totalballs = batsmanStrikeRateData[key][keys]['Balls']
+                let strikerate = (batsmanStrikeRateData[key][keys]['Runs'] / totalballs) * 100
+                batsmanStrikeRateData[key][keys] = strikerate;
+
+            }
         }
-        //creating a new object and pushing the strikerate of each batsman based on year
-        for(let keys in object[key]){
-            let totalballs=object[key][keys]['Balls']-object[key][keys]['Wide']
-            let strikerate=(object[key][keys]['Runs']/totalballs)*100
-            object1[key][keys]=strikerate;
-            
-        }
+        return batsmanStrikeRateData;
     }
-    return object1;
-    
+    catch (Error) {
+        console.log(Error)
+    }
+
 }
