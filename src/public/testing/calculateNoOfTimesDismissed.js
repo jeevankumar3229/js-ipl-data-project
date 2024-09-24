88c5aaef6db9cf16384a409530a38fc9403f304b
Jeevan Kumar <jeevankumar3229@gmail.com>
2024-09-24 22:17:20 +0530

    Modify function using array reduce method, modify variable names, add required comments and add try catch code

--- src/public/testing/calculateNoOfTimesDismissed.js
+++ src/public/testing/calculateNoOfTimesDismissed.js
@@ -1,5 +1,7 @@
 import calculateNoOfTimesDismissed from "../../server/8-highest-no-of-times-dismissed.js";
-import fs from "fs";
-let output= calculateNoOfTimesDismissed()
+import readingDeliveriesData from "./readingDeliveriesData.js";
+import writingData from "./writingData.js";
+let deliveriesData= readingDeliveriesData()//calling function to read deliveries data
+let output= calculateNoOfTimesDismissed(deliveriesData)//this function returns the data of the player dismissed multiple times by the same player
 let jsondata=JSON.stringify(output,null,2)
-fs.writeFile('./src/public/output/highestNoOfTimesDismissed.json',jsondata,(err)=>{if(err)console.log(err.message)});
+writingData('./src/public/output/highestNoOfTimesDismissed.json',jsondata)//calling function to write data to the json file
--- src/server/8-highest-no-of-times-dismissed.js
+++ src/server/8-highest-no-of-times-dismissed.js
@@ -1,71 +1,74 @@
-import fs from "fs"
-export default function calculateNoOfTimesDismissed(){
-    let bowlerArray=[]
-    let fileteredArray=[]
-    let object1={}
-    let object={}
-    let data=JSON.parse(fs.readFileSync('./src/data/deliveries.json','utf-8',(err)=>{if(err) console.log("Error")}));
-    //pushing only those rows to array which contains the dismissal kind as runout,caught and lbw
-    for(let index=0;index<data.length;index++){
-        if(data[index]["dismissal_kind"]==='caught' || data[index]["dismissal_kind"]==='run out' || data[index]["dismissal_kind"]==='lbw'){
-            fileteredArray.push(data[index])
+//this function calculates the data of the player dismissed multiple times by the same player
+export default function calculateNoOfTimesDismissed(deliveriesData) {
+    try {
+        let bowlerArray = []
+        let filteredDeliveriesData = []
+        let noOfTimesDismissedData = {}
+        //pushing only those rows to array which contains the dismissal kind as runout,caught and lbw
+        for (let index = 0; index < deliveriesData.length; index++) {
+            if (deliveriesData[index]["dismissal_kind"] === 'caught' || deliveriesData[index]["dismissal_kind"] === 'run out' || deliveriesData[index]["dismissal_kind"] === 'lbw') {
+                filteredDeliveriesData.push(deliveriesData[index])
+            }
         }
-    }
-    //pushing the names of the bowler to array
-    for(let index1=0;index1<fileteredArray.length;index1++){
-        if(bowlerArray.includes(fileteredArray[index1]['bowler'])){
-            continue;
+        //pushing the names of the bowler to array
+        for (let index = 0; index < filteredDeliveriesData.length; index++) {
+            if (bowlerArray.includes(filteredDeliveriesData[index]['bowler'])) {
+                continue;
+            }
+            else {
+                bowlerArray.push(filteredDeliveriesData[index]['bowler'])
+            }
         }
-        else{
-            bowlerArray.push(fileteredArray[index1]['bowler'])
-        }
-    }
-    for(let index2=0;index2<bowlerArray.length;index2++){
-        object[bowlerArray[index2]]={}
-        for(let index3=0;index3<fileteredArray.length;index3++){
-            if(fileteredArray[index3].hasOwnProperty("bowler")){
-                if(fileteredArray[index3]['bowler'] === bowlerArray[index2]){
-                    if(object[bowlerArray[index2]].hasOwnProperty(fileteredArray[index3]['batsman'])){
-                        object[bowlerArray[index2]][fileteredArray[index3]['batsman']]++;
-                    }
-                    else{
-                        object[bowlerArray[index2]][fileteredArray[index3]['batsman']]=1;
+        //noOfTimesDismissedData object contains keys as bowler name and value as nested object, nested object contains batsman name as key and value as no of times dismissed
+        for (let index = 0; index < bowlerArray.length; index++) {
+            noOfTimesDismissedData[bowlerArray[index]] = {}
+            for (let index1 = 0; index1 < filteredDeliveriesData.length; index1++) {
+                if (filteredDeliveriesData[index1].hasOwnProperty("bowler")) {
+                    if (filteredDeliveriesData[index1]['bowler'] === bowlerArray[index]) {
+                        if (noOfTimesDismissedData[bowlerArray[index]].hasOwnProperty(filteredDeliveriesData[index1]['batsman'])) {
+                            noOfTimesDismissedData[bowlerArray[index]][filteredDeliveriesData[index1]['batsman']]++;
+                        }
+                        else {
+                            noOfTimesDismissedData[bowlerArray[index]][filteredDeliveriesData[index1]['batsman']] = 1;
+                        }
                     }
                 }
             }
         }
-    }
 
-    let playerName;
-    let maxvalue=0;
-    //to convert set of batsman in each baller as a key to one batsman with most no of times dismissed
-    for(let key1 in object){
-        for(let key2 in object[key1]){
-            if(maxvalue<object[key1][key2]){
-                maxvalue=object[key1][key2]
-                playerName=key2
+        let batsmanNames;
+        let dismissedCount = 0;
+        //to convert set of batsman in each baller as a key to one batsman with most no of times dismissed
+        for (let bowlerName in noOfTimesDismissedData) {
+            for (let batsmanName in noOfTimesDismissedData[bowlerName]) {
+                if (dismissedCount < noOfTimesDismissedData[bowlerName][batsmanName]) {
+                    dismissedCount = noOfTimesDismissedData[bowlerName][batsmanName]
+                    batsmanNames = batsmanName
+                }
+
             }
-            
+            noOfTimesDismissedData[bowlerName] = { "name": batsmanNames, "noOfTimesDismissed": dismissedCount }
+            dismissedCount = 0;
         }
-        object[key1]={"name":playerName,"noofTimesDismissed": maxvalue}
-        maxvalue=0;
-    }
-    let ballerName=""
-    playerName=""
-    //to get the player with max no of times dismissed
-    for(let key1 in object){
-        for(let key2 in object[key1]){
-            if(maxvalue<object[key1][key2]){
-                maxvalue=object[key1][key2]
-                playerName=object[key1]["name"]
-                ballerName=key1;
+        let bowlerNames = ""
+        batsmanNames = ""
+        //to get the player with max no of times dismissed
+        for (let bowlerName in noOfTimesDismissedData) {
+            for (let batsmanName in noOfTimesDismissedData[bowlerName]) {
+                if (dismissedCount < noOfTimesDismissedData[bowlerName][batsmanName]) {
+                    dismissedCount = noOfTimesDismissedData[bowlerName][batsmanName]
+                    batsmanNames = noOfTimesDismissedData[bowlerName]["name"]
+                    bowlerNames = bowlerName;
+                }
             }
-        } 
-    }
-    //creating new object which only contains the name of batsman with no of times dismissed  by same player
-    object1[ballerName]={"name":playerName,"noofTimesDismissed": maxvalue}
-    
-    return object1;
+        }
+        noOfTimesDismissedData = {}
+        noOfTimesDismissedData[bowlerNames] = { "name": batsmanNames, "noOfTimesDismissed": dismissedCount }
 
-    
+        return noOfTimesDismissedData;
+    }
+    catch (Error) {
+        console.log(Error)
+    }
+
 }
